3f1c6958de7d6e5cdd21ec53d47eb84af424e671
gavxin <380274+gavxin@users.noreply.github.com>
2021-11-06 17:26:40 +0800

    update tmux config

--- config_tmux.sh
+++ config_tmux.sh
@@ -2,5 +2,7 @@ echo config tmux guide: https://github.com/gpakosz/.tmux
 echo
 echo uncomment tmux_conf_theme_left_separator_XXX to enable powerline
 echo
+echo disable C-a as send-prefix
+echo
 echo DO NOT forget export EDITOR=vim
 echo 
